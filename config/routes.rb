51175778ad69f55a05c8e00912cf1ada37c06b16
emferrari31 <e.ferrari@live.co.uk>
2024-11-01 15:47:32 +0000

    project complete

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get "/users/sign_out" => 'devise/sessions#destroy'
+
+  end
+  resources :friends
+  # get "home/index"
+  get 'home/about'
+  # root 'home#index'
+  root 'friends#index'
+  get "up" => "rails/health#show", as: :rails_health_check
+
+end
