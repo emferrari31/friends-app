7cb89806e9913cee999481b7d34b325b317209b2
emferrari31 <e.ferrari@live.co.uk>
2024-11-01 16:22:01 +0000

    linting properly resolved

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get "/users/sign_out" => 'devise/sessions#destroy'
-
+    get "/users/sign_out" => "devise/sessions#destroy"
   end
   resources :friends
   # get "home/index"
-  get 'home/about'
-  # root 'home#index'
-  root 'friends#index'
+  get "home/about"
+  # root "home#index"
+  root "friends#index"
   get "up" => "rails/health#show", as: :rails_health_check
-
 end
